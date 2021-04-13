91bb9ed3dd19a15e53ea8b00e121cf744bc77372
JhayDaivd <jdrodriguez5930@misena.edu.co>
2021-04-13 12:09:17 -0500

    :

--- sistema/js/ajax_formularios/alert.js
+++
@@ -1,9 +0,0 @@
-function alerta(){
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Reporte Generado Correctamente',
-        showConfirmButton: false,
-        timer: 3000
-      })
-  }
