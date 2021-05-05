34d2301bc11475caef84e2289517f7e808914d63
Cristian18-code <cristiangarzon180@gmail.com>
2021-05-05 11:36:27 -0500

    cambio para conexion local con base de datos

--- config/db.php
+++ config/db.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'gestion_contact');//Nombre de la base de datos
+define('DB_NAME', 'gestion_contact_c');//Nombre de la base de datos
 ?>
